f7a2a88e64b8aac4652335be13ac20bdeb66b99e
Soboruuu <116648895+Soboruuu@users.noreply.github.com>
2023-02-14 20:03:59 +0900

    Add files via upload

---
+++ day02/2_days_weeks_months_counter.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+days = (90*365)-int(age)*365
+weeks = (90*52)-int(age)*52
+months = (90*12)-int(age)*12
+
+print(f"you have {days} days, {weeks} weeks, {months} months left.")
+
+
+
+
+# 365 days a year, 52 weeks a year, 12 months in a year.
+# you have xxxxx days, xxxx weeks and xxx months left.
