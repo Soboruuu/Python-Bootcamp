b9108a45192bcce3412c79e757e5a7f1b456b4c6
Soboruuu <116648895+Soboruuu@users.noreply.github.com>
2023-02-22 21:55:35 +0900

    Add files via upload

---
+++ day16/main.py
@@ -0,0 +1,22 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+options = menu.get_items()
+
+is_turned_off = False
+
+while not is_turned_off:
+    order = input(f"What would you like? {options}: ").lower()
+    if order == "off":
+        is_turned_off = True
+    elif order == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        coffee = menu.find_drink(order)
+        if coffee_maker.is_resource_sufficient(coffee) and money_machine.make_payment(coffee.cost):
+            coffee_maker.make_coffee(coffee)
